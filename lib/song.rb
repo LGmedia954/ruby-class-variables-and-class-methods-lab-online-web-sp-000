730c0c863e474488f3ceaf12d49c23911f293f08
Leslie G <lesliegonzalez@me.com>
2020-07-31 02:13:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ def self.artists
   
   def self.artist_count
     artist_count = {}
-    artists.each do |artist|
+    @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1
       else
